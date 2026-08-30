c97185b716e98b43577593e6208e14844ba267c5
Muhammad Waqas <muhammadwaqas.dev99@gmail.com>
2026-08-30 19:32:04 +0500

    feat: add signup form UI

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,16 @@
+
+import { Montserrat } from "next/font/google";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+
+
 import "./globals.css";
+
+
+const montserrat =Montserrat({
+  subsets:["latin"]
+})
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,7 +33,10 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      
+<body className={`${montserrat.className} min-h-full flex flex-col`}>
+  {children}
+</body>
     </html>
   );
 }
---
+++ app/signup/page.tsx
@@ -0,0 +1,71 @@
+import Link from "next/link";
+
+export default function Signup() {
+  return (
+    <div className="flex min-h-screen items-center justify-center">
+      <form className="flex w-full max-w-md flex-col items-center bg-black rounded-4xl p-8 shadow-2xl gap-3">
+        <h1 className="font-bold text-3xl text-white ">Signup</h1>
+        <p className="text-sm  text-center   w-full text-[#9A9A9A] ">
+          Enter your details to create your account
+        </p>
+
+        <div className="flex flex-col items-start w-full px-8 mb-1.5 mt-3">
+          <label htmlFor="username" className="text-white text-sm ">
+            Username
+          </label>
+          <input
+            type="text"
+            id="username"
+            placeholder="john doe"
+            className=" w-full p-3  mt-1 rounded-lg bg-[#1C1C1C]             transition duration-200
+           hover:bg-[#242424]
+           focus:outline-none
+           focus:ring-2 focus:ring-yellow-300"
+          />
+        </div>
+
+        <div className="flex flex-col items-start w-full px-8 mb-1.5 mt-3">
+          <label htmlFor="email" className="text-white text-sm ">
+            Email
+          </label>
+          <input
+            type="email"
+            id="email"
+            placeholder="youremail@gmail.com"
+            className=" w-full p-3  mt-1 rounded-lg bg-[#1C1C1C]            transition duration-200
+           hover:bg-[#242424]
+           focus:outline-none
+           focus:ring-2 focus:ring-yellow-300 "
+          />
+        </div>
+
+        <div className="flex flex-col items-start w-full px-8 mb-1.5 mt-3">
+          <label htmlFor="password" className="text-white text-sm ">
+            Password
+          </label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            placeholder="*******"
+            className=" w-full p-3  mt-1 rounded-lg bg-[#1C1C1C]            transition duration-200
+           hover:bg-[#242424]
+           focus:outline-none
+           focus:ring-2 focus:ring-yellow-300"
+          />
+        </div>
+        <div className="px-8 w-full mt-4">
+          <button className="w-full  py-3 rounded-lg  text-black  font-bold text-lg bg-yellow-400 hover:bg-yellow-500 ">
+            Signup
+          </button>
+        </div>
+        <p className="text-sm  text-center   w-full text-[#9A9A9A] ">
+          Already have an account?{" "}
+          <Link href="/login" className="text-yellow-300 hover:underline ml-1">
+            Login
+          </Link>
+        </p>
+      </form>
+    </div>
+  );
+}
