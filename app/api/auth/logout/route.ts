f06e7cd43f5f19daf0cb2c2600bfdf04ed2dcd4b
Muhammad Waqas <muhammadwaqas.dev99@gmail.com>
2026-09-14 21:37:27 +0500

    feat(auth): implement logout and refresh token revocation

--- app/api/auth/logout/route.ts
+++ app/api/auth/logout/route.ts
@@ -1,7 +1,32 @@
+import connectToDB from "@/app/dbconfig/db";
+import { hashRefreshToken } from "@/app/lib/auth/token";
+import RefreshToken from "@/app/models/refreshToken.model";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(request: NextRequest) {
   try {
+
+    const refreshToken = request.cookies.get('refreshToken')?.value.toString()
+          if (  !refreshToken) {
+    return NextResponse.json({ error: 'Unauthorized' }, { status: 401 })
+  }
+
+   const hashedRefreshToken = hashRefreshToken(refreshToken).toString()
+await connectToDB()
+   const checkRefreshToken = await RefreshToken.findOne({tokenHash:hashedRefreshToken})
+ 
+   if(!checkRefreshToken || checkRefreshToken.revokedAt !==null){
+    return NextResponse.json({success:false , message : "invalid Refresh Token "} , {status: 401})
+   }
+
+
+   checkRefreshToken.revokedAt = new Date()
+
+   await checkRefreshToken.save()
+
+
+   
+
     const response = NextResponse.json(
       {
         success: true,
@@ -10,7 +35,10 @@ export async function POST(request: NextRequest) {
       { status: 200 }
     );
 
-    response.cookies.delete("token");
+    response.cookies.delete("refreshToken");
+    response.cookies.delete("accessToken");
+
+
 
     return response;
   } catch (error: any) {
